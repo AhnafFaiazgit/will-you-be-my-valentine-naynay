a514247570c25c8a40adff316bb125e7f19c8844
unknown <rajiasultan60@gmail.com>
2026-02-01 17:54:00 +0600

    Initial commit with local edits

---
+++ src/App.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { useState } from "react";
+
+export default function Page() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "What if I asked really nicely?",
+      "Pretty please",
+      "PLEASEEEEEEEEE",
+      "What if I make you nuggets",
+      "PLEASE MEOW",
+      "NOOOOO :(((((",
+      "NOOOOO I CANT TAKE THIS MORI JAIMU",
+      "Ok mori gesi",
+      "You're litterally talking to Ahnafs's ghost",
+      "please shunaaa",
+      ":((((",
+      "PRETTY PLEASEEEEE",
+      "Kandidimu",
+      "No :(",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  return (
+    <div className="-mt-16 flex h-screen flex-col items-center justify-center">
+      {yesPressed ? (
+        <>
+          <img src="https://tenor.com/view/kiss-anime-cute-gif-23821845" />
+          <div className="my-4 text-4xl font-bold">YIPPPEEEE I LOVE YOU MOYNA MWAH MWAH ;))</div>
+        </>
+      ) : (
+        <>
+          <img
+            className="h-[200px]"
+            src="https://tenor.com/view/cat-flowers-cat-with-flowers-cat-flowers-cute-gif-4015690078339243957"
+          />
+          <h1 className="my-4 text-4xl">Will you be my Valentine NayNay?</h1>
+          <div className="flex items-center">
+            <button
+              className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/will-you-be-my-valentine-naynay/', // <-- your repo name here
+});
